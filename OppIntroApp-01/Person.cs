1b609f5c632650c00b359e272cf7768b18768708
istiaqhaider8 <istiaqhaider8@gmail.com>
2022-07-16 17:05:14 +0600

    OOP in class

---
+++ OppIntroApp-01/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OppIntroApp_01
+{
+    class Person
+    {
+
+        public String firstName;
+        public String middleName;
+        public String lastName;
+
+        public string GetFullName()
+        {
+            string fullName = firstName + " " + middleName + " " + lastName;
+            return fullName;
+        }
+
+        public string GetReverseName()
+        {
+            string fullName = GetFullName();
+            char[] cArray = fullName.ToCharArray();
+            string reverse = String.Empty;
+            for(int i = cArray.Length-1; i >- 1; i--)
+            {
+                reverse += cArray[i];
+            }
+            return reverse;
+        }
+
+    }
+        
+}
---
+++ OppIntroApp-01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OppIntroApp_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Person aPerson = new Person();
+            aPerson.firstName = "Istiaq";
+            aPerson.middleName = "Haider";
+            aPerson.lastName = "Hridon";
+
+            String fullName = aPerson.GetFullName();
+            Console.WriteLine(fullName);
+
+
+            String reverseName = aPerson.GetReverseName();
+            Console.WriteLine(reverseName);
+
+            Console.ReadLine();
+        
+
+         
+        }
+    }
+}
